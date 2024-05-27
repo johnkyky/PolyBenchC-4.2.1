b185b0b6856b38d69bd29690d2291a4fc1b3bd8e
johnkyky <ugo.battiston@protonmail.com>
2024-05-27 14:19:36 +0200

    adding medley kernels (without the kokkos convertion)

---
+++ medley/deriche/deriche.cpp
@@ -0,0 +1,192 @@
+/**
+ * This version is stamped on May 10, 2016
+ *
+ * Contact:
+ *   Louis-Noel Pouchet <pouchet.ohio-state.edu>
+ *   Tomofumi Yuki <tomofumi.yuki.fr>
+ *
+ * Web address: http://polybench.sourceforge.net
+ */
+/* deriche.c: this file is part of PolyBench/C */
+
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* Include polybench common header. */
+#include <polybench.h>
+
+/* Include benchmark-specific header. */
+#include "deriche.h"
+
+/* Array initialization. */
+static void init_array(int w, int h, DATA_TYPE *alpha,
+                       ARRAY_2D_FUNC_PARAM(DATA_TYPE, imgIn, W, H, w, h),
+                       ARRAY_2D_FUNC_PARAM(DATA_TYPE, imgOut, W, H, w, h)) {
+  *alpha = 0.25; // parameter of the filter
+
+  // input should be between 0 and 1 (grayscale image pixel)
+  for (int i = 0; i < w; i++)
+    for (int j = 0; j < h; j++)
+      ARRAY_2D_ACCESS(imgIn, i, j) =
+          (DATA_TYPE)((313 * i + 991 * j) % 65536) / 65535.0f;
+}
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static void print_array(int w, int h,
+                        ARRAY_2D_FUNC_PARAM(DATA_TYPE, imgOut, W, H, w, h)) {
+  POLYBENCH_DUMP_START;
+  POLYBENCH_DUMP_BEGIN("imgOut");
+  for (int i = 0; i < w; i++)
+    for (int j = 0; j < h; j++) {
+      if ((i * h + j) % 20 == 0)
+        fprintf(POLYBENCH_DUMP_TARGET, "\n");
+      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER,
+              ARRAY_2D_ACCESS(imgOut, i, j));
+    }
+  POLYBENCH_DUMP_END("imgOut");
+  POLYBENCH_DUMP_FINISH;
+}
+
+/* Main computational kernel. The whole function will be timed,
+   including the call and return. */
+/* Original code provided by Gael Deest */
+static void kernel_deriche(int w, int h, DATA_TYPE alpha,
+                           ARRAY_2D_FUNC_PARAM(DATA_TYPE, imgIn, W, H, w, h),
+                           ARRAY_2D_FUNC_PARAM(DATA_TYPE, imgOut, W, H, w, h),
+                           ARRAY_2D_FUNC_PARAM(DATA_TYPE, y1, W, H, w, h),
+                           ARRAY_2D_FUNC_PARAM(DATA_TYPE, y2, W, H, w, h)) {
+  DATA_TYPE xm1, tm1, ym1, ym2;
+  DATA_TYPE xp1, xp2;
+  DATA_TYPE tp1, tp2;
+  DATA_TYPE yp1, yp2;
+
+  DATA_TYPE k;
+  DATA_TYPE a1, a2, a3, a4, a5, a6, a7, a8;
+  DATA_TYPE b1, b2, c1, c2;
+
+#pragma scop
+  k = (SCALAR_VAL(1.0) - EXP_FUN(-alpha)) *
+      (SCALAR_VAL(1.0) - EXP_FUN(-alpha)) /
+      (SCALAR_VAL(1.0) + SCALAR_VAL(2.0) * alpha * EXP_FUN(-alpha) -
+       EXP_FUN(SCALAR_VAL(2.0) * alpha));
+  a1 = a5 = k;
+  a2 = a6 = k * EXP_FUN(-alpha) * (alpha - SCALAR_VAL(1.0));
+  a3 = a7 = k * EXP_FUN(-alpha) * (alpha + SCALAR_VAL(1.0));
+  a4 = a8 = -k * EXP_FUN(SCALAR_VAL(-2.0) * alpha);
+  b1 = POW_FUN(SCALAR_VAL(2.0), -alpha);
+  b2 = -EXP_FUN(SCALAR_VAL(-2.0) * alpha);
+  c1 = c2 = 1;
+
+  for (int i = 0; i < _PB_W; i++) {
+    ym1 = SCALAR_VAL(0.0);
+    ym2 = SCALAR_VAL(0.0);
+    xm1 = SCALAR_VAL(0.0);
+    for (int j = 0; j < _PB_H; j++) {
+      ARRAY_2D_ACCESS(y1, i, j) =
+          a1 * ARRAY_2D_ACCESS(imgIn, i, j) + a2 * xm1 + b1 * ym1 + b2 * ym2;
+      xm1 = ARRAY_2D_ACCESS(imgIn, i, j);
+      ym2 = ym1;
+      ym1 = ARRAY_2D_ACCESS(y1, i, j);
+    }
+  }
+
+  for (int i = 0; i < _PB_W; i++) {
+    yp1 = SCALAR_VAL(0.0);
+    yp2 = SCALAR_VAL(0.0);
+    xp1 = SCALAR_VAL(0.0);
+    xp2 = SCALAR_VAL(0.0);
+    for (int j = _PB_H - 1; j >= 0; j--) {
+      ARRAY_2D_ACCESS(y2, i, j) = a3 * xp1 + a4 * xp2 + b1 * yp1 + b2 * yp2;
+      xp2 = xp1;
+      xp1 = ARRAY_2D_ACCESS(imgIn, i, j);
+      yp2 = yp1;
+      yp1 = ARRAY_2D_ACCESS(y2, i, j);
+    }
+  }
+
+  for (int i = 0; i < _PB_W; i++)
+    for (int j = 0; j < _PB_H; j++) {
+      ARRAY_2D_ACCESS(imgOut, i, j) =
+          c1 * (ARRAY_2D_ACCESS(y1, i, j) + ARRAY_2D_ACCESS(y2, i, j));
+    }
+
+  for (int j = 0; j < _PB_H; j++) {
+    tm1 = SCALAR_VAL(0.0);
+    ym1 = SCALAR_VAL(0.0);
+    ym2 = SCALAR_VAL(0.0);
+    for (int i = 0; i < _PB_W; i++) {
+      ARRAY_2D_ACCESS(y1, i, j) =
+          a5 * ARRAY_2D_ACCESS(imgOut, i, j) + a6 * tm1 + b1 * ym1 + b2 * ym2;
+      tm1 = ARRAY_2D_ACCESS(imgOut, i, j);
+      ym2 = ym1;
+      ym1 = ARRAY_2D_ACCESS(y1, i, j);
+    }
+  }
+
+  for (int j = 0; j < _PB_H; j++) {
+    tp1 = SCALAR_VAL(0.0);
+    tp2 = SCALAR_VAL(0.0);
+    yp1 = SCALAR_VAL(0.0);
+    yp2 = SCALAR_VAL(0.0);
+    for (int i = _PB_W - 1; i >= 0; i--) {
+      ARRAY_2D_ACCESS(y2, i, j) = a7 * tp1 + a8 * tp2 + b1 * yp1 + b2 * yp2;
+      tp2 = tp1;
+      tp1 = ARRAY_2D_ACCESS(imgOut, i, j);
+      yp2 = yp1;
+      yp1 = ARRAY_2D_ACCESS(y2, i, j);
+    }
+  }
+
+  for (int i = 0; i < _PB_W; i++)
+    for (int j = 0; j < _PB_H; j++)
+      ARRAY_2D_ACCESS(imgOut, i, j) =
+          c2 * (ARRAY_2D_ACCESS(y1, i, j) + ARRAY_2D_ACCESS(y2, i, j));
+
+#pragma endscop
+}
+
+int main(int argc, char **argv) {
+
+  INITIALIZE;
+
+  /* Retrieve problem size. */
+  int w = W;
+  int h = H;
+
+  /* Variable declaration/allocation. */
+  DATA_TYPE alpha;
+  POLYBENCH_2D_ARRAY_DECL(imgIn, DATA_TYPE, W, H, w, h);
+  POLYBENCH_2D_ARRAY_DECL(imgOut, DATA_TYPE, W, H, w, h);
+  POLYBENCH_2D_ARRAY_DECL(y1, DATA_TYPE, W, H, w, h);
+  POLYBENCH_2D_ARRAY_DECL(y2, DATA_TYPE, W, H, w, h);
+
+  /* Initialize array(s). */
+  init_array(w, h, &alpha, POLYBENCH_ARRAY(imgIn), POLYBENCH_ARRAY(imgOut));
+
+  /* Start timer. */
+  polybench_start_instruments;
+
+  /* Run kernel. */
+  kernel_deriche(w, h, alpha, POLYBENCH_ARRAY(imgIn), POLYBENCH_ARRAY(imgOut),
+                 POLYBENCH_ARRAY(y1), POLYBENCH_ARRAY(y2));
+
+  /* Stop and print timer. */
+  polybench_stop_instruments;
+  polybench_print_instruments;
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  polybench_prevent_dce(print_array(w, h, POLYBENCH_ARRAY(imgOut)));
+
+  /* Be clean. */
+  POLYBENCH_FREE_ARRAY(imgIn);
+  POLYBENCH_FREE_ARRAY(imgOut);
+  POLYBENCH_FREE_ARRAY(y1);
+  POLYBENCH_FREE_ARRAY(y2);
+
+  FINALIZE;
+
+  return 0;
+}
---
+++ medley/nussinov/nussinov.cpp
@@ -0,0 +1,141 @@
+/**
+ * This version is stamped on May 10, 2016
+ *
+ * Contact:
+ *   Louis-Noel Pouchet <pouchet.ohio-state.edu>
+ *   Tomofumi Yuki <tomofumi.yuki.fr>
+ *
+ * Web address: http://polybench.sourceforge.net
+ */
+/* nussinov.c: this file is part of PolyBench/C */
+
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/* Include polybench common header. */
+#include <polybench.h>
+
+/* Include benchmark-specific header. */
+#include "nussinov.h"
+
+/* RNA bases represented as chars, range is ,0,3) */
+typedef char base;
+
+#define match(b1, b2) (((b1) + (b2)) == 3 ? 1 : 0)
+#define max_score(s1, s2) ((s1 >= s2) ? s1 : s2)
+
+/* Array initialization. */
+static void init_array(int n, ARRAY_1D_FUNC_PARAM(base, seq, N, n),
+                       ARRAY_2D_FUNC_PARAM(DATA_TYPE, table, N, N, n, n)) {
+  // base is AGCT/0..3
+  for (int i = 0; i < n; i++) {
+    ARRAY_1D_ACCESS(seq, i) = (base)((i + 1) % 4);
+  }
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+      ARRAY_2D_ACCESS(table, i, j) = 0;
+}
+
+/* DCE code. Must scan the entire live-out data.
+   Can be used also to check the correctness of the output. */
+static void print_array(int n,
+                        ARRAY_2D_FUNC_PARAM(DATA_TYPE, table, N, N, n, n)) {
+  int t = 0;
+
+  POLYBENCH_DUMP_START;
+  POLYBENCH_DUMP_BEGIN("table");
+  for (int i = 0; i < n; i++) {
+    for (int j = i; j < n; j++) {
+      if (t % 20 == 0)
+        fprintf(POLYBENCH_DUMP_TARGET, "\n");
+      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER,
+              ARRAY_2D_ACCESS(table, i, j));
+      t++;
+    }
+  }
+  POLYBENCH_DUMP_END("table");
+  POLYBENCH_DUMP_FINISH;
+}
+
+/* Main computational kernel. The whole function will be timed,
+   including the call and return. */
+/*
+  Original version by Dave Wonnacott at Haverford College
+  <davew@cs.haverford.edu>, with help from Allison Lake, Ting Zhou, and Tian
+  Jin, based on algorithm by Nussinov, described in Allison Lake's senior
+  thesis.
+*/
+static void kernel_nussinov(int n, ARRAY_1D_FUNC_PARAM(base, seq, N, n),
+                            ARRAY_2D_FUNC_PARAM(DATA_TYPE, table, N, N, n, n)) {
+#pragma scop
+  for (int i = _PB_N - 1; i >= 0; i--) {
+    for (int j = i + 1; j < _PB_N; j++) {
+
+      if (j - 1 >= 0)
+        ARRAY_2D_ACCESS(table, i, j) = max_score(
+            ARRAY_2D_ACCESS(table, i, j), ARRAY_2D_ACCESS(table, i, j - 1));
+      if (i + 1 < _PB_N)
+        ARRAY_2D_ACCESS(table, i, j) = max_score(
+            ARRAY_2D_ACCESS(table, i, j), ARRAY_2D_ACCESS(table, i + 1, j));
+
+      if (j - 1 >= 0 && i + 1 < _PB_N) {
+        /* don't allow adjacent elements to bond */
+        if (i < j - 1)
+          ARRAY_2D_ACCESS(table, i, j) = max_score(
+              ARRAY_2D_ACCESS(table, i, j),
+              ARRAY_2D_ACCESS(table, i + 1, j - 1) +
+                  match(ARRAY_1D_ACCESS(seq, i), ARRAY_1D_ACCESS(seq, j)));
+        else
+          ARRAY_2D_ACCESS(table, i, j) =
+              max_score(ARRAY_2D_ACCESS(table, i, j),
+                        ARRAY_2D_ACCESS(table, i + 1, j - 1));
+      }
+
+      for (int k = i + 1; k < j; k++) {
+        ARRAY_2D_ACCESS(table, i, j) = max_score(
+            ARRAY_2D_ACCESS(table, i, j),
+            ARRAY_2D_ACCESS(table, i, k) + ARRAY_2D_ACCESS(table, k + 1, j));
+      }
+    }
+  }
+#pragma endscop
+}
+
+int main(int argc, char **argv) {
+
+  INITIALIZE;
+
+  /* Retrieve problem size. */
+  int n = N;
+
+  /* Variable declaration/allocation. */
+  POLYBENCH_1D_ARRAY_DECL(seq, base, N, n);
+  POLYBENCH_2D_ARRAY_DECL(table, DATA_TYPE, N, N, n, n);
+
+  /* Initialize array(s). */
+  init_array(n, POLYBENCH_ARRAY(seq), POLYBENCH_ARRAY(table));
+
+  /* Start timer. */
+  polybench_start_instruments;
+
+  /* Run kernel. */
+  kernel_nussinov(n, POLYBENCH_ARRAY(seq), POLYBENCH_ARRAY(table));
+
+  /* Stop and print timer. */
+  polybench_stop_instruments;
+  polybench_print_instruments;
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(table)));
+
+  /* Be clean. */
+  POLYBENCH_FREE_ARRAY(seq);
+  POLYBENCH_FREE_ARRAY(table);
+
+  FINALIZE;
+
+  return 0;
+}
