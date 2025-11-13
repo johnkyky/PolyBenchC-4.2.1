1181c915a072245a9abf51277c8ac83959cb8e26
johnkyky <ugo.battiston@protonmail.com>
2025-11-13 13:12:59 +0100

    converte kernel to polly

--- datamining/covariance/covariance.cpp
+++ datamining/covariance/covariance.cpp
@@ -52,7 +52,33 @@ static void kernel_covariance(size_t m, size_t n, DATA_TYPE float_n,
                               ARRAY_2D_FUNC_PARAM(DATA_TYPE, data, N, M, n, m),
                               ARRAY_2D_FUNC_PARAM(DATA_TYPE, cov, M, M, m, m),
                               ARRAY_1D_FUNC_PARAM(DATA_TYPE, mean, M, m)) {
-#if defined(POLYBENCH_KOKKOS)
+#if defined(POLYBENCH_USE_POLLY)
+  const auto policy_1D = Kokkos::RangePolicy<>(0, m);
+  const auto policy_2D = Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {n, m});
+
+  Kokkos::parallel_for<usePolyOpt,
+                       "p0.l0 == 0, p0.l0 == p2.l0, p0.u0 == p2.u0, p1.l0 == "
+                       "0, p1.l1 == 0, p1.u1 == p0.u0">(
+      "kernel", policy_1D,
+      KOKKOS_LAMBDA(const size_t j) {
+        mean(j) = SCALAR_VAL(0.0);
+        for (size_t i = 0; i < n; i++)
+          mean(j) += data(i, j);
+        mean(j) /= float_n;
+      },
+      policy_2D,
+      KOKKOS_LAMBDA(const size_t i, const size_t j) { data(i, j) -= mean(j); },
+      policy_1D,
+      KOKKOS_LAMBDA(const size_t i) {
+        for (long j = i; j < m; j++) {
+          cov(i, j) = SCALAR_VAL(0.0);
+          for (long k = 0; k < n; k++)
+            cov(i, j) += data(k, i) * data(k, j);
+          cov(i, j) /= (float_n - SCALAR_VAL(1.0));
+          cov(j, i) = cov(i, j);
+        }
+      });
+#elif defined(POLYBENCH_KOKKOS)
   const auto policy_1D = Kokkos::RangePolicy<>(0, m);
   const auto policy_2D_1 =
       Kokkos::MDRangePolicy<Kokkos::Rank<2>>({0, 0}, {n, m});
@@ -81,14 +107,6 @@ static void kernel_covariance(size_t m, size_t n, DATA_TYPE float_n,
           cov(j, i) = cov(i, j);
         }
       });
-  // Kokkos::parallel_for(
-  //     policy_2D_2, KOKKOS_LAMBDA(const size_t i, const size_t j) {
-  //       cov(i, j) = SCALAR_VAL(0.0);
-  //       for (size_t k = 0; k < n; k++)
-  //         cov(i, j) += data(k, i) * data(k, j);
-  //       cov(i, j) /= (float_n - SCALAR_VAL(1.0));
-  //       cov(j, i) = cov(i, j);
-  //     });
 #else
 #pragma scop
   for (size_t j = 0; j < m; j++) {
