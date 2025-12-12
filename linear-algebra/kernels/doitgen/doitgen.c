3078c50aef0c2a8d9263ae198dcc80629b8a0e17
johnkyky <ugo.battiston@protonmail.com>
2025-12-12 18:31:17 +0100

    convert kernel to polly

--- linear-algebra/kernels/doitgen/doitgen.c
+++ linear-algebra/kernels/doitgen/doitgen.c
@@ -1,7 +1,7 @@
 void kernel_doitgen(int nr, int nq, int np, double A[nr][nq][np],
                     double tmp[nr][nq][np], double C4[np][np], double sum[np]) {
 #pragma scop
-  for (int r = 0; r < nr; r++)
+  for (int r = 0; r < nr; r++) {
     for (int q = 0; q < nq; q++) {
       for (int p = 0; p < np; p++) {
         sum[p] = 0.0;
@@ -11,5 +11,6 @@ void kernel_doitgen(int nr, int nq, int np, double A[nr][nq][np],
       for (int p = 0; p < np; p++)
         A[r][q][p] = sum[p];
     }
+  }
 #pragma endscop
 }
